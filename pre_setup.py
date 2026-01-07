00e17354b69d7cef44c0d39cf5961eb19fd24875
Jason Ogbebor <81113182+JOgbebor@users.noreply.github.com>
2026-01-07 13:46:27 -0500

    Update development Test Driver URL

--- pre_setup.py
+++ pre_setup.py
@@ -25,7 +25,7 @@ OPENKIM_TEST_DRIVERS = {
 
 # List of URLs of development Test Drivers to test
 DEVEL_TEST_DRIVERS = {
-    "FreeEnergies-000b0.tar.gz":
+    "FreeEnergies-000b2.tar":
         {"target_size": 100}
 }
 
