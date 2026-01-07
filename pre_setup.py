83da2a7c1e9edcc456d7b79cfbd0d35067958271
Jason Ogbebor <81113182+JOgbebor@users.noreply.github.com>
2026-01-07 12:05:48 -0500

    Update development test driver URL format

--- pre_setup.py
+++ pre_setup.py
@@ -24,7 +24,7 @@ OPENKIM_TEST_DRIVERS = {
 }
 
 # List of URLs of development Test Drivers to test
-DEVEL_TEST_DRIVERS = {"https://github.com/openkim-hackathons/FreeEnergies__TD_000000000000_000/archive/refs/tags/v000b0.tar.gz"}
+DEVEL_TEST_DRIVERS = {"FreeEnergies-000b0.tar"}
 
 MAX_URLLIB_ATTEMPTS = 10
 
